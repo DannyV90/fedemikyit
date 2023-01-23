042007cbc9335c790b93bb41edab0f6091d79bb6
DannyV90 <danielnoize90@gmail.com>
2023-01-23 17:26:57 +0100

    Ottimizzazione grafica

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -17,7 +17,7 @@ class FormController extends Controller
      */
     public function __invoke(MessageRequest $request)
     {
-        Mail::to('danielnoize@gmail.com')->send(new Dedication(
+        Mail::to('danielnoize90@gmail.com')->send(new Dedication(
             $request->validated('name') ,
             $request->validated('email'),
             $request->validated('phone'),
