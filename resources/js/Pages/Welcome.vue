cb73bbbbe9df50fc86245b6053f3dfc61e2dfb15
DannyV90 <danielnoize90@gmail.com>
2023-01-24 13:28:31 +0100

    Completato

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -74,7 +74,7 @@ function submit() {
         <title>Ci Sposiamo</title>
     </Head>
 
-    <div id="mainDiv" class="flex flex-col justify-center bg-fuchsia-50 min-h-screen py-16 text-center "
+    <div v-cloak id="mainDiv" class="flex flex-col justify-center bg-fuchsia-50 min-h-screen py-16 text-center "
          style="color: rgb(89,77,53)">
         <section class="z-50 h-screen mb-32 lg:mb-60 mt-4">
             <div class="flex flex-col xl:flex-row items-center justify-center text-3xl md:text-6xl xl:text-8xl
@@ -286,6 +286,10 @@ function submit() {
     </div>
 </template>
 <style scoped>
+
+[v-cloak]{
+    display: none;
+}
 
 #mainDiv{
     position: relative;
