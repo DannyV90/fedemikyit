66bdc47d341dbefaaa168305fbca5027f4ff2117
DannyV90 <danielnoize90@gmail.com>
2023-02-16 13:23:47 +0100

    Sistemazione layout completata

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -123,8 +123,8 @@ const closeModal = () => {
              style="font-family: 'SophiaMartini', cursive"
              data-aos="fade-in"
              data-aos-easing="ease-out-cubic"
-             data-aos-delay="300"
-             data-aos-offset="300"
+             data-aos-delay="500"
+             data-aos-offset="400"
              data-aos-duration="3000"
         >
             <div class="flex flex-col gap-y-6 md:gap-y-14 xl:flex-row z-50">
@@ -179,7 +179,7 @@ const closeModal = () => {
                     data-aos="flip-right"
                     data-aos-easing="ease-out-cubic"
                     data-aos-delay="300"
-                    data-aos-offset="100"
+                    data-aos-offset="300"
                     data-aos-duration="3000"
                 >
             </div>
@@ -205,7 +205,7 @@ const closeModal = () => {
             data-aos="fade-down"
             data-aos-easing="ease-out-cubic"
             data-aos-delay="300"
-            data-aos-offset="100"
+            data-aos-offset="300"
             data-aos-duration="3000"
         >
         <img
@@ -279,7 +279,7 @@ const closeModal = () => {
             data-aos="zoom-in"
             data-aos-easing="ease-out-cubic"
             data-aos-delay="500"
-            data-aos-offset="300"
+            data-aos-offset="100"
             data-aos-duration="3000"
         >
             <Countdown
@@ -294,7 +294,7 @@ const closeModal = () => {
             data-aos="zoom-out"
             data-aos-easing="ease-out-cubic"
             data-aos-delay="500"
-            data-aos-offset="300"
+            data-aos-offset="100"
             data-aos-duration="3000"
         >
             <div class="text-2xl md:text-3xl">
